cbb50785aee571604aae57a2741bd38e1709e6d2
Sumit134coder <73213318+Sumit134coder@users.noreply.github.com>
2021-01-11 13:27:30 +0530

    styles

--- app.js
+++ app.js
@@ -5,10 +5,15 @@ var tasks = document.querySelector("#tasks");
 function TaskAdded(){
     var node = document.createElement("li");
     var btnDone = document.createElement("button");
-    btnDone.innerHTML = "DONE";
+    btnDone.innerHTML = '<i class="fas fa-check"></i>';
     var textNode = document.createTextNode(AddTask.value);
     node.appendChild(textNode);
+    var btnTrash = document.createElement("button");
+    btnTrash.innerHTML = '<i class="fas fa-trash"></i>'
+    //apending the buttonn and task
     document.getElementById("tasks").appendChild(node).appendChild(btnDone);
+    document.getElementById("tasks").appendChild(node).appendChild(btnTrash);
+
     AddTask.value = "";
     btnDone.addEventListener("click", ()=>{
         document.getElementById("tasks").removeChild(node);
