71d96a98d8a152db065580759111157425e9ab77
Sumit134coder <73213318+Sumit134coder@users.noreply.github.com>
2021-01-11 14:16:33 +0530

    fix: button textnode

--- app.js
+++ app.js
@@ -9,13 +9,19 @@ function TaskAdded(){
     var textNode = document.createTextNode(AddTask.value);
     node.appendChild(textNode);
     var btnTrash = document.createElement("button");
-    btnTrash.innerHTML = '<i class="fas fa-trash"></i>'
+    btnTrash.innerHTML = '<i class="fas fa-trash"></i>';
+    btnTrash.classList.add("btn-Trash");
     //apending the buttonn and task
     document.getElementById("tasks").appendChild(node).appendChild(btnDone);
     document.getElementById("tasks").appendChild(node).appendChild(btnTrash);
 
     AddTask.value = "";
-    btnDone.addEventListener("click", ()=>{
+    btnDone.addEventListener("click", (node)=>{
+        var item =document.getElementById("tasks").node;
+        item.classList.add("completed")
+
+    })
+    btnTrash.addEventListener("click",()=>{
         document.getElementById("tasks").removeChild(node);
     })
 }
